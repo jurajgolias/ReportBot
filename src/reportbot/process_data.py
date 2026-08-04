5a14c46d95ca94a3e8584fe8a3841047c1949479
Juraj Goliaš <jurajgolias0@gmail.com>
2026-08-04 13:10:44 +0200

    style: format project with ruff

--- src/reportbot/process_data.py
+++ src/reportbot/process_data.py
@@ -6,7 +6,5 @@ def process_weather_data(raw_data: dict) -> dict:
         "date": daily_data["time"][0],
         "maximum_temperature": daily_data["temperature_2m_max"][0],
         "minimum_temperature": daily_data["temperature_2m_min"][0],
-        "rain_probability": daily_data[
-            "precipitation_probability_max"
-        ][0],
+        "rain_probability": daily_data["precipitation_probability_max"][0],
     }
